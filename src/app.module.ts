82687c3237a824e88e87b282f5645aed6aec14c6
Robinson De La Cruz <rdelacruzsiado@gmail.com>
2023-07-23 17:18:24 -0500

    feat: instalar algunas dependencias para el manejo de variables de entorno, cookies y estrategias de autenticación

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 import { UserModule } from './user/infrastructure/user.module';
 import { PublicationModule } from './publication/infraestructure/publication.module';
+import { AuthModule } from './auth/auth.module';
+import config from './config';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/knowme_db'),
+    ConfigModule.forRoot({
+      envFilePath: `.env`,
+      load: [config],
+      isGlobal: true,
+    }),
     UserModule,
     PublicationModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,6 +15,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
 
   SwaggerModule.setup('docs', app, document);
+
+  app.use(cookieParser());
 
   await app.listen(3000);
 }
